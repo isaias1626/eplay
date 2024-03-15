70f0dace07708cbe84d005c7cbca80ab7ca9b6a2
¨isaias <isaiasrocha1626@gmail.com>
2024-03-15 18:39:41 -0300

    criando home

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,29 @@
+import { Imagem, Precos, Titulo } from './styles'
+
+import bannerImg from '../../Assets/images/banner-homem-aranha.png'
+import Tag from '../Tag'
+import Button from '../button'
+
+const Banner = () => (
+  <Imagem style={{ backgroundImage: `url(${bannerImg})` }}>
+    <div className="container">
+      <Tag size="big">Destaque do dia </Tag>
+      <div>
+        <Titulo> Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Titulo>
+        <Precos>
+          De <span>R$ 250,00</span> <br />
+          por apenas R$ 99,90
+        </Precos>
+      </div>
+      <Button
+        type="link"
+        to="/produto"
+        title="clique aqui para aproveitar essa oferta"
+      >
+        Aproveitar
+      </Button>
+    </div>
+  </Imagem>
+)
+
+export default Banner
